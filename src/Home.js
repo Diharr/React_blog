8b2b873081bf03929f3f0f8b4de27ce94dc060fa
Dihsar <ackrashid@gmail.com>
2023-04-04 12:59:36 -0400

    Finished the basics of the blog

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
+import BlogList from "./Bloglist";
+import useFetch from "./useFetch";
+
 const Home = () => {
+    const {data, isPending, error} = useFetch('http://localhost:8000/blogs');
+ 
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading....</div>}
+            {data && <BlogList blogs={data} title="All Blogs" />}
         </div>  
      );
 }
